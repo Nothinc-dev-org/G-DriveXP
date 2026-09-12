64aae2a004a30c8e778a4b3b090394bded7d03d6
alcss <fernandoalcalacasas@hotmail.com>
2026-09-12 17:33:52 -0600

    v1.1.0: install.sh usa PNG único 256x256 del tarball

--- g-drive-xp/packaging/install.sh
+++ g-drive-xp/packaging/install.sh
@@ -55,9 +55,7 @@ info "Instalando archivo .desktop..."
 sudo install -Dm644 "$SRC/org.gnome.FedoraDrive.desktop" /usr/share/applications/org.gnome.FedoraDrive.desktop
 
 info "Instalando iconos..."
-sudo install -Dm644 "$SRC/org.gnome.FedoraDrive-48.png" /usr/share/icons/hicolor/48x48/apps/org.gnome.FedoraDrive.png
-sudo install -Dm644 "$SRC/org.gnome.FedoraDrive-128.png" /usr/share/icons/hicolor/128x128/apps/org.gnome.FedoraDrive.png
-sudo install -Dm644 "$SRC/org.gnome.FedoraDrive-256.png" /usr/share/icons/hicolor/256x256/apps/org.gnome.FedoraDrive.png
+sudo install -Dm644 "$SRC/org.gnome.FedoraDrive.png" /usr/share/icons/hicolor/256x256/apps/org.gnome.FedoraDrive.png
 for emblem in "$SRC"/emblem-gdrivexp-*.svg; do
     sudo install -Dm644 "$emblem" "/usr/share/icons/hicolor/scalable/emblems/$(basename "$emblem")"
 done
